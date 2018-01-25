a6b7758f45c7ee00d6e343e639cd84e62f461414
Alan Thompson <cloojure@gmail.com>
2018-01-25 09:37:12 -0800

    misc

--- resources/leiningen/new/tupelo_app/core_tst.clj
+++ resources/leiningen/new/tupelo_app/core_tst.clj
@@ -14,5 +14,9 @@
     (is (truthy? (re-find #"THESE are the times" crisis-txt))))
 
   (is= 5.0 (Calc/add 2 3))
+
+  (let [result ({{name}}.Calc/incVals {"able" 1 "baker" 2} ) ]
+    (is= {"able" 2 "baker" 3} (spyxx result))
+    (is= java.util.HashMap (type result)))
 )
 
